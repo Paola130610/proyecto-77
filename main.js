ab8188424c2bc8f8073af0411abcd4c6f1b0edf0
Paola130610 <108499719+Paola130610@users.noreply.github.com>
2022-09-26 19:36:34 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("Hola yo soy la pestaña de la consola");
+var primer_nombre = "Paola";
+var apellido = "Fernanda";
+var nombre_completo = primer_nombre.concat(apellido);
+console.log("nombre completo: "+nombre_completo);
+var a = 10;
+var b = 5;
+var sumar = a + b;
+console.log("Sumar: "+sumar);
+var restar = a - b;
+console.log("Restar: "+restar);
+var multiplicacion = a * b ;
+console.log("Multiplicacion: "+multiplicacion)
+var dividir = a / b ;
+console.log("Divicion: "+dividir)
